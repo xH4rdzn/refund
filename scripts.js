a83966fe9ec79be9fefb96df09f82215cd1cc8ee
Igor Coelho <xh4rdzn@gmail.com>
2025-03-17 21:40:51 -0300

    feat: function to add new expenses

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@ const amount = document.getElementById('amount')
 const form = document.querySelector('form')
 const expense = document.getElementById('expense')
 const category = document.getElementById('category')
-
+const expenseList = document.querySelector('ul')
+const expenseQuantity = document.querySelector("aside header p span")
 
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "")
@@ -34,5 +35,58 @@ form.onsubmit = (event) => {
     created_at: new Date(),
   }
 
-  console.log(newExpense)
+  expenseAdd(newExpense)
+}
+
+
+function expenseAdd(newExpense) {
+  try {
+    const expenseItem = document.createElement('li')
+    expenseItem.classList.add('expense')
+
+    const expenseIcon = document.createElement('img')
+    expenseIcon.setAttribute('src', `img/${newExpense.category_id}.svg`)
+    expenseIcon.setAttribute('alt', newExpense.category_name)
+
+    const expenseInfo = document.createElement("div")
+    expenseInfo.classList.add('expense-info')
+
+    const expenseName = document.createElement("strong")
+    expenseName.textContent = newExpense.expense
+
+    const expenseCategory = document.createElement("span")
+    expenseCategory.textContent = newExpense.category_name
+
+    expenseInfo.append(expenseName, expenseCategory)
+
+    const expenseAmount = document.createElement('span')
+    expenseAmount.classList.add('expense-amount')
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace("R$", "")}`
+
+    const removeIcon = document.createElement("img")
+    removeIcon.classList.add('remove-icon')
+    removeIcon.setAttribute("src", "img/remove.svg")
+    removeIcon.setAttribute("alt", "remover")
+
+
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
+    expenseList.append(expenseItem)
+
+    updateTotals()
+
+  } catch (error) {
+    alert('Não foi possível atualizar a lista de despesas.')
+    console.log(error)
+  }
+}
+
+function updateTotals() {
+  try {
+    const items = expenseList.children
+
+    expenseQuantity.textContent = `${items.length} ${items.length > 1 ? "despesas": "despesa"}`
+  } catch (error) {
+    alert("Não foi possível atualizar os totais")
+    console.log(error)
+  }
 }
